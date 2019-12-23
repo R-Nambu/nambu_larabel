d933cbded93793a5334df8b7c10d30821f7a1243
南部遼 <>
2019-12-23 13:53:36 +0800

    11th commit
    画像登録機能の追加

--- Diary/app/Http/Controllers/Auth/RegisterController.php
+++ Diary/app/Http/Controllers/Auth/RegisterController.php
@@ -52,7 +52,19 @@ class RegisterController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
+            'picture' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048'
         ]);
+    }
+
+    private function saveProfileImage($image)
+    {
+        // デフォルトではstorage/appに画像が保存されます。 
+        // 第2引数にpublicをつけることで、storage/app/publicに保存されます。 
+        // 今回は、/images/profilePictureをつけて、
+        // storage/app/public/images/profilePictureに画像が保存されるようにしています。
+        // 自分で指定しない場合、ファイル名は自動で設定されます。  
+        $imgPath = $image->store('images/profilePicture', 'public');
+        return 'storage/' . $imgPath;
     }
 
     /**
@@ -63,10 +75,12 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        $imgPath = $this->saveProfileImage($data['picture']);
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'picture_path' => $imgPath,
         ]);
     }
 }
